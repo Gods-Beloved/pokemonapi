cb77edee183651e153453d7a6d5cafb68730e68b
James Godwill Arkoh <jamesgodwillarkoh@gmail.com>
2024-03-26 22:50:01 +0000

    Established connection to postgre pokemon database

--- src/main/java/com/dev/james/pokemonapi/models/Review.java
+++ src/main/java/com/dev/james/pokemonapi/models/Review.java
@@ -1,15 +1,24 @@
 package com.dev.james.pokemonapi.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Review {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String title;
     private String content;
-    private int starts;
+    private int start;
 
 }
