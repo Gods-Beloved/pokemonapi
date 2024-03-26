1b46654c246d59f5f7842021d6b855b8affd0133
James Godwill Arkoh <jamesgodwillarkoh@gmail.com>
2024-03-26 14:00:44 +0000

    created pokemon api starter project

---
+++ src/test/java/com/dev/james/pokemonapi/PokemonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.james.pokemonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
